bb0ff599da4f66c9486fdd7d9c82cb1fabdac83a
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2023-01-30 21:16:58 +0300

    final

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,8 +4,10 @@ from rest_framework import permissions
 class OnlyAuthorPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS or \
-               obj.author == request.user
+        return (
+                request.method in permissions.SAFE_METHODS or
+                obj.author == request.user
+        )
 
     def __str__(self):
         return 'У вас недостаточно прав для выполнения данного действия.'
